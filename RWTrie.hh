fe63c060d54f6a74aeb08ba5c0db8bfae84c0119
liugetu <168144675+liugetu@users.noreply.github.com>
2025-10-04 13:50:36 +0200

    update rwtrie.hh

--- RWTrie.hh
+++ RWTrie.hh
@@ -71,6 +71,8 @@ public:
 
     //T getValue(string key);
 
+    node* delete_key(node* n, string key);
+
     /*Arbre() {
         primer_node= NULL;
     }
@@ -150,26 +152,6 @@ public:
     bool es_buit() const {
         return (primer_node==NULL);
     }*/
-
-    node* delete_key(node* n, string key) {
-        if(n == nullptr) return nullptr;
-        else if(key.empty()) n->info = NULL;
-        //recursively delete from children
-        else n->phijos[key[0]] = delete_node(n->phijos[key[0]], key.substr(1));
-        
-        //if current node still has value, keep it
-        if(n->info != NULL) return n;
-        
-        //if current node is prefix for another key, keep it
-        for (auto child : n->phijos) {
-            if (child != nullptr) return n;
-        }
-
-        //if current node doesn't have value and is not prefix, delete it
-        delete n;
-        return nullptr;
-    }
-
 
 };
 
