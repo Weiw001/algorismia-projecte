973e151229fa87b6b30aeae98a80f0ad47c1f188
J-ZP <javier.zhangpan@estudiantat.upc.edu>
2025-10-04 15:08:46 +0200

    fix compilation errors

--- RWTrie1.cc
+++ RWTrie1.cc
@@ -1,19 +1,6 @@
 #include "RWTrie.hh"
 
-<<<<<<< HEAD
-template<class T> int RWTrie<T>::recursiveFind(RWTrie<T>::node* raiz, int index, const string &x) {
-=======
-<<<<<<< HEAD
-
-
-
-
 template <typename T>int RWTrie<T>:: recursiveFind(node* raiz, int index, const string &x) {
-=======
-int recursiveFind(node* raiz, int index, const string &x) {
->>>>>>> eb8bde7514cbaa861621c9ea1f125e5559d6d245
->>>>>>> refs/remotes/origin/main
-
     if(index != x.length() - 1) {
         // before verifying the  last character 
         node* next = raiz[x[index]-'0'];
@@ -27,11 +14,7 @@ int recursiveFind(node* raiz, int index, const string &x) {
     }
 }
 
-<<<<<<< HEAD
-template<class T> T RWTrie<T>::getValue(const string &x) {
-=======
 template <typename T> T RWTrie<T>::getValue(const string &x) {
->>>>>>> refs/remotes/origin/main
     if (raiz->phijos[x[0] - '0'] == NULL) return T();
     int result;
     int index = 0;
@@ -44,12 +27,6 @@ template <typename T> T RWTrie<T>::getValue(const string &x) {
     return result;
 }
 
-<<<<<<< HEAD
-template<class T> bool RWTrie<T>::contains(string key) {
-    return getValue(key) != NULL;
-=======
-
 template <typename T> bool RWTrie<T>:: contains(string key) {
     return (getValue(key) != T());
->>>>>>> refs/remotes/origin/main
 }
