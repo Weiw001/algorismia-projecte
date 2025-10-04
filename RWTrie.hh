372d6bf0a3b04a2acfe4f89f2dad87ed16ca584b
J-ZP <javier.zhangpan@estudiantat.upc.edu>
2025-10-04 13:14:33 +0200

    replaced default constructor of T with NULL

--- RWTrie.hh
+++ RWTrie.hh
@@ -134,12 +134,12 @@ public:
 
     node* delete_key(node* n, string key) {
         if(n == nullptr) return nullptr;
-        else if(key.empty()) n->info = T();
+        else if(key.empty()) n->info = NULL;
         //recursively delete from children
         else n->phijos[key[0]] = delete_node(n->phijos[key[0]], key.substr(1));
         
         //if current node still has value, keep it
-        if(n->info != T()) return n;
+        if(n->info != NULL) return n;
         
         //if current node is prefix for another key, keep it
         for (auto child : n->phijos) {
